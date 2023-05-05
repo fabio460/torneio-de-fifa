e63cedb1092c499468f2956fc9407d85befd0a49
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-05-05 00:10:44 -0300

    projeto teste de deplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import TelaPrincipal from './telaPrincipal'
+import RotaPrivada from './rotaPrivada'
+import Login from './login'
+import Cadastro from './cadastro'
+import TelaDeElenco from './telaDeElenco'
+import TelaCompraDeJogadores from './telaCompraDeJogadores'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<RotaPrivada><TelaPrincipal/></RotaPrivada>}></Route>
+              <Route path='/login' element={<Login/>}></Route>
+              <Route path='/elenco' element={<RotaPrivada><TelaDeElenco/></RotaPrivada>}></Route>
+              <Route path='/compraDeJogadores' element={<RotaPrivada><TelaCompraDeJogadores/></RotaPrivada>}></Route>
+              <Route path='/cadastro' element={<Cadastro/>}></Route>
+          </Routes>
+        </BrowserRouter>  
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/telaDeElenco/optCampLista.tsx
@@ -0,0 +1,103 @@
+
+
+
+import  React,{useEffect,useState} from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import QuatroUmDoisTres from './escalacoes/quatroUmDoisTres';
+import Reservas from './escalacoes/quatroUmDoisTres/reservas';
+import { participantesType } from '../types';
+import TabelaDeJogadores from './tabelaDeJogadores';
+import { listarParticipantesApi } from '../api/participantesApi';
+
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+export default function OptCampoLista({handlePosition, elenco}:{handlePosition:any, elenco:participantesType | undefined}) {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+ 
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+
+  
+  return (
+    <Box sx={{ bgcolor: 'white', width: "100%" }}>
+      <div>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="standard"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Lista" {...a11yProps(0)} />
+          <Tab label="Campinho" {...a11yProps(1)} />
+          <Tab label="Time" {...a11yProps(2)} />
+        </Tabs>
+      </div>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          <TabelaDeJogadores jogadores={elenco?.jogadores} elenco={elenco}/>
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          <div className='campinhoBody'>
+              <QuatroUmDoisTres handlePosition={handlePosition} jogadores={elenco?.jogadores}/>
+              <div className='reservarContainer'><Reservas handlePosition={handlePosition} jogadores={elenco?.jogadores}/></div>
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
+    </Box>
+  );
+}
